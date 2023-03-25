cf708c862c4c44e42a1ba156950888e9f6684a93
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2023-03-26 00:46:25 +0200

    Removed multiline space indent marks (except .py)

--- lua/akk/options.lua
+++ lua/akk/options.lua
@@ -1,55 +1,60 @@
 -- AKK nvim options
+local opt = vim.opt
 
 -- Enable syntax highlighting.
 vim.cmd('syn on')
 
 -- Line numbering as relative to current line.
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Always pad with <value> number of lines vertically.
-vim.opt.scrolloff = 10
+opt.scrolloff = 10
 
 -- Smart-ish language agnostic indenting behaviour.
-vim.opt.smartindent = true
+opt.smartindent = true
 
 -- Tabs expand to spaces
-vim.opt.expandtab = true
+opt.expandtab = true
 
 -- Set (auto)indenting step count as spaces (4 spaces as 1 tab).
-vim.opt.shiftwidth = 4
+opt.shiftwidth = 4
 
 -- Number of spaces a <Tab> counts for in a file.
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+opt.tabstop = 4
+opt.softtabstop = 4
 
 -- Disable wrapping when lines clip over viewport.
-vim.opt.wrap = false
+opt.wrap = false
 
 -- Highlight search matches. Clear highlight with :noh.
-vim.opt.hlsearch = true
+opt.hlsearch = true
 
 -- (OFF) Ignore search case. Can be beneficial to switch off.
-vim.opt.ignorecase = true
+opt.ignorecase = true
 
 -- VSplit to right side, HSplit below
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Whitespace formatting. Helps noticing style errors. Multispace
 -- added mainly to differentiate space indenting from tabs.
-vim.opt.list = true
-vim.opt.listchars = { trail = '~', tab = '>-', multispace = '---+' }
+opt.list = true
+opt.listchars = { trail = '~', tab = '>-', multispace = '---+', lead = ' ' }
 
 -- Instead of closing buffers on switching from them, hide them.
-vim.opt.hidden = true
+opt.hidden = true
 
 -- Write buffers on almost all changes in window state.
-vim.opt.autowriteall = true
+opt.autowriteall = true
 
 -- If changes to a file have been made outside Vim, reread the file
 -- automatically.
-vim.opt.autoread = true
+opt.autoread = true
 
--- Suppress stupid press-enter-prompts.
-vim.opt.shortmess = 'at'
+-- Shorten the time of swap backups
+opt.updatetime = 50
+
+-- Length of wait between sequential map keystrokes
+opt.timeout = true
+opt.timeoutlen = 300
