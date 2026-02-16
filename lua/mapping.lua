0867ca14e22b818e8c0fc124861223771f0dd156
Erkka Lehtoranta <erkka.lehtoranta@nokia.com>
2026-02-16 11:14:16 +0200

    Add git mappings

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -30,13 +30,22 @@ vim.keymap.set('n', '<leader>cd', ':cclose<CR>')
 vim.keymap.set('n', '<M-j>', 'jzz')
 vim.keymap.set('n', '<M-k>', 'kzz')
 
+-- Yank all
 vim.keymap.set('n', '<leader>ya', ':%y<CR>')
+
+-- Git
+vim.keymap.set('n', '<leader>ga', ':Git add %<CR>')
+vim.keymap.set('n', '<leader>gc', ':Git commit<CR>')
+vim.keymap.set('n', '<leader>gs', ':Git status<CR>')
+vim.keymap.set('n', '<leader>gdd', ':Gdiffsplit!<CR>')
+vim.keymap.set('n', '<leader>gdl', ':diffget //2<CR>')
+vim.keymap.set('n', '<leader>gdh', ':diffget //3<CR>')
 
 -- LSP
 -- Replace with LspAttach event autocmd if errors
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)           -- ReName
 vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references)       -- List references
-vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition)       -- Go to definition
+vim.keymap.set('n', '<leader>dd', vim.lsp.buf.definition)       -- Go to definition
 vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation)   -- List implementation
 vim.keymap.set('n', '<leader>jt', vim.lsp.buf.type_definition)  -- Jump to type
 vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float)    -- Diagnostic float
